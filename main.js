d4e99bc308b8b20529c7efd17fb669991863bc6b
blyxyas <blyxyas@gmail.com>
2023-06-23 10:53:43 +0200

    fix v2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Initial setup
 
